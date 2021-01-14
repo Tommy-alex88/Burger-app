262cfcaf44a8f7bf12c29cf3aaa986d3d36c4667
tommy-alex88 <tommy.alex88@gmail.com>
2021-01-14 20:34:02 +0700

    burgerIngredient added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 
 import Auxiliary from '../../hoc/Auxiliary';
+import BurgerIngredient from '../../components/Burger/BurgerIngredient/BurgerIngredient';
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Auxiliary>
-        <div>Burger</div>
+        <BurgerIngredient className='bred-top' />
+        <BurgerIngredient className='cheese' />
+        <BurgerIngredient className='meat' />
+        <BurgerIngredient className='salad' />
+        <BurgerIngredient className='bred-bottom' />
         <div>Build Controls</div>
       </Auxiliary>
     )
