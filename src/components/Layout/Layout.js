ce7c5f614a6301471299ef37c24a15d48beccca7
tommy-alex88 <tommy.alex88@gmail.com>
2021-02-01 12:23:30 +0700

    SideDrawer in process

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
-  <Auxiliary>
-    <div> Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Auxiliary>
+	<Auxiliary>
+		<Toolbar />
+		<SideDrawer />
+		<main className={classes.Content}>{props.children}</main>
+	</Auxiliary>
 );
 
 export default layout;
